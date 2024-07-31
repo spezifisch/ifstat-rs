17d304a80fe3399b579232fc3a67b1bd7f34eb9b
spezifisch <spezifisch+git@gmail.com>
2024-07-31 09:11:45 +0200

    fix vergen build info when not in a git repo, like in cargo install

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
+use std::path::Path;
 use vergen::{vergen, Config};
 
 fn main() {
-    vergen(Config::default()).expect("Unable to generate build information");
+    if Path::new(".git").exists() {
+        vergen(Config::default()).expect("Unable to generate build information");
+    } else {
+        println!(
+            "cargo:warning=No .git directory found. Skipping vergen build information generation."
+        );
+    }
 }
